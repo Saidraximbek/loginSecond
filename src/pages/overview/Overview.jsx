1da2d08678e65c634b52d996e778687461db9c53
saidrxim <saidrxim@gmail.com>
2025-04-23 03:25:43 +0500

    feat: Enhance sidebar and layout styles for better responsiveness
    - Updated Sidebar.module.scss to adjust media query breakpoints and added new styles for nav items.
    - Modified MainLayout.module.scss to ensure full width for the main container.
    - Refactored Overview.jsx to integrate collections data and improve layout with new components.
    - Created Overview.module.scss with styles for the overview page, including responsive design adjustments.
    - Adjusted _resets.scss for consistent width settings across the application.
    - Implemented useCollection.js hook for fetching Firestore collections.
    - Developed useCollectionsData.js hook to fetch multiple collections and manage loading state.

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
 import style from "./Overview.module.scss"
+import { useCollectionsData } from '../../hooks/useCollectionsData'
+import { FaCaretRight } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 const Overview = () => {
+ const {data, isPending} =  useCollectionsData()
+ console.log(data);
+ 
   return (
-   <div>Overview</div>
+   <div className={style.overviewContainer}>
+    <h1 className={style.overTitle}>Overview</h1>
+    <div className={style.overviewContent}>
+      
+    <ul className={style.overAmounts}>
+      <li className={style.overAmountsItem}>
+        <p className={style.currentBalanceTitle}>Current Balance</p>
+        <p className={style.currentBalance}>${data && data.balance.current}.00</p>
+      </li>
+      <li className={style.overAmountsItem}>
+        <p className={style.currentBalanceTitle}>Income</p>
+        <p className={style.currentBalance}>${data && data.balance.income}</p>
+      </li>
+      <li className={style.overAmountsItem}>
+        <p className={style.currentBalanceTitle}>Expenses</p>
+        <p className={style.currentBalance}>${data && data.balance.expenses
+        }</p>
+      </li>
+    </ul>
+
+    <div className={style.overAllInfos}>
+      <div className={style.potsTran}>
+
+      <div className={style.pots}>
+        <div className={style.potsTitleView}>
+
+        <h2 className={style.potsTitle}>Pots</h2>
+        <NavLink to="/posts">
+
+        <p className={style.viewPots}>See Details <FaCaretRight /></p>
+        </NavLink>
+        </div>
+
+      </div>
+      <div className={style.tran}>tran</div>
+      </div>
+
+      <div className={style.budgetBills}>
+        <div className={style.budgets}>budget</div>
+        <div className={style.bills}>bills</div>
+      </div>
+    </div>
+    </div>
+   </div>
   )
 }
 
